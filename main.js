de31f1b773db55249e0df31695d83afac32c05b1
Aditya Jha <59570577+adityakumarjha@users.noreply.github.com>
2022-11-09 20:37:12 +0530

    Commit

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var attempt = 3;
-let btn_element = document.getElementByID("submit");
+let btn_element = document.getElementById("submit");
   
 btn_element.addEventListener("click",validate()) // Variable to count number of attempts.
 console.log(username+"test");// Below function Executes on click of login button.
 function validate(){
-var username = document.getElementByID("uname").value;
-var password = document.getElementByID("psw").value;
+var username = document.getElementById("uname").value;
+var password = document.getElementById("psw").value;
 console.log(username+"test");
 if ( username == "test" && password == "admin"){
 alert ("Login successfully");
@@ -17,9 +17,9 @@ attempt --;// Decrementing by one.
 alert("You have left "+attempt+" attempt;");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
-document.getElementByID("uname").disabled = true;
-document.getElementByID("psw").disabled = true;
-document.getElementByID("submit").disabled = true;
+document.getElementById("uname").disabled = true;
+document.getElementById("psw").disabled = true;
+document.getElementById("submit").disabled = true;
 return false;
 }
 }
