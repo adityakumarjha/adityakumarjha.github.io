bcd51bae6fda281d6d096531ea992dd0c32d0dbb
Aditya Jha <59570577+adityakumarjha@users.noreply.github.com>
2022-11-12 22:04:32 +0530

    commit

---
+++ LandingPagePHC/main.js
@@ -0,0 +1,31 @@
+
+if(localStorage.getItem("attempt")==null)
+{
+localStorage.setItem("attempt", 3);
+}
+else
+attempt=localStorage.getItem("attempt");
+//var btn_element = document.getElementById("submit");
+  
+//btn_element.addEventListener("click",validate) // Variable to count number of attempts.
+console.log(username+"test");// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("uname").value;
+var password = document.getElementById("psw").value;
+console.log(username+"test");
+if ( username == "test" && password == "admin"){
+    window.location.replace('https://adityakumarjha.github.io/LandingPHC.html'); // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("uname").disabled = true;
+document.getElementById("psw").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
