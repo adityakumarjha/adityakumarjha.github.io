06177173d1dc4ace98ed7be338c7e8b58f37b211
Aditya Jha <59570577+adityakumarjha@users.noreply.github.com>
2022-11-20 10:07:55 +0530

    commit

---
+++ LandingPOC/main.js
@@ -0,0 +1,36 @@
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
+  }
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " container";
+}
+
+var buildingList = [{
+    "BuildingName": "Adler Journalism and Mass Communication Building",
+        "BuildingNumber": "0456"
+}, {
+    "BuildingName": "Art Building",
+        "BuildingNumber": "0021"
+}];
+
+var dropDown = document.getElementById('symptoms');
+
+// iterate through the list
+for (i = 0; i < buildingList.length; i++) {
+
+    // create a new element
+    var listItem = document.createElement("li");
+
+    // set the value 
+    listItem.innerHTML = '<a role="menuitem" href=' + buildingList[i].BuildingName + '>' + buildingList[i].BuildingName + '</a>'
+
+    // append the item to the list
+    dropDown.appendChild(listItem);
+}
